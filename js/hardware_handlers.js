8389b1d7c263b7ba5aaf3e8fcc9f6b5856f5533a
Adam Drew <adam.drew@nimblestorage.com>
2017-09-14 17:07:37 -0400

    Initial commit. Started with the Tizen Tau basic app template and refactored it a bit. Moved the hardware handler boilerplate into a dedicated js file. Removed the reference to tau.min.js in index.

---
+++ js/hardware_handlers.js
@@ -0,0 +1,81 @@
+( function () {
+	//Add any custom init code here
+	
+	//Back button event handler
+	window.addEventListener( 'tizenhwkey', function( ev ) {
+		if( ev.keyName === "back" ) {
+			var page = document.getElementsByClassName( 'ui-page-active' )[0],
+				pageid = page ? page.id : "";
+			if( pageid === "main" ) {
+				try {
+					tizen.application.getCurrentApplication().exit();
+				} catch (ignore) {
+				}
+			} else {
+				window.history.back();
+			}
+		}
+	} );
+	
+	//Low battery event handler
+	var systeminfo = {
+
+		systeminfo: null,
+
+		lowThreshold : 0.04,
+
+		listenBatteryLowState: function(){
+			var self = this;
+			try {
+				this.systeminfo.addPropertyValueChangeListener(
+					'BATTERY',
+					function change(battery){
+						if(!battery.isCharging) {
+							try {
+								tizen.application.getCurrentApplication().exit();
+							} catch (ignore) {
+							}
+						}
+					},
+					{
+						lowThreshold : self.lowThreshold
+					},
+					console.warn("An error occurred")
+				);
+			} catch (ignore) {
+			}
+		},
+
+		checkBatteryLowState: function(){
+			var self = this;
+			try {
+				this.systeminfo.getPropertyValue(
+					'BATTERY',
+					function(battery) {
+						if(battery.level < self.lowThreshold && !battery.isCharging) {
+							try {
+								tizen.application.getCurrentApplication().exit();
+							} catch (ignore) {
+							}
+						}
+					},
+					null);
+			} catch (ignore) {
+			}
+		},
+
+		init: function(){
+			if (typeof tizen === 'object' && typeof tizen.systeminfo === 'object'){
+				this.systeminfo = tizen.systeminfo;
+				this.checkBatteryLowState();
+				this.listenBatteryLowState();
+			}
+			else{
+				console.warn('tizen.systeminfo is not available.');
+			}
+		}
+	};
+
+	systeminfo.init();
+	
+} () );
