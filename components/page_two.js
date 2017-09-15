4222e7be1105f408df6a311610b84ba85f13fc3d
Adam Drew <adamrdrew@live.com>
2017-09-15 15:59:03 -0400

    More example code in components

--- components/page_two.js
+++ components/page_two.js
@@ -4,7 +4,34 @@ const pageTwo = {
 							<h2 class='ui-title'>Page 2</h2>\
 						</header>\
 						<div class='ui-content content-padding'>\
-							<p>Hello Page 2!</p><router-link to='/one'>One</router-link>\
+							<ul class='ui-listview'>\
+								<li><p>Hello Page 2!</p></li>\
+								<li><router-link to='/one'>One</router-link></li>\
+								<li v-on:click='saveFile'>Save File</li>\
+								<li v-on:click='loadFile'>Load File</li>\
+								<li>Loading: {{loading}}</li>\
+								<li>Save: {{testData.fileSaveWorked}}</li>\
+							</ul>\
 						</div>\
-					</div>"
+					</div>",
+		data: function() {
+			return {
+				testData: {fileSaveWorked: false},
+				loading: false
+			}
+		},
+		methods: {
+			saveFile: function() {
+				var fileManager = new VueZen.FileManager("wgt-private/newFilePath.txt");
+				fileManager.saveJSON({fileSaveWorked: true});
+			},
+			loadFile: function() {
+				var fileManager = new VueZen.FileManager("wgt-private/newFilePath.txt");
+				this.loading = true;
+				fileManager.readJSON(function(result){
+					this.testData.fileSaveWorked = result.fileSaveWorked;
+					this.loading = false;
+				}.bind(this));				
+			}
+		}
 };
