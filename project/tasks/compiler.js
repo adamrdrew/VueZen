f1516e662777b801ef1fb195c94ca3f24e5444cc
Adam Drew <adamrdrew@live.com>
2017-09-18 19:03:04 -0400

    Refactored compiler. Moved tag and component functionality into their own classes. Tried to make the code more clear and easier to reason about.

--- project/tasks/compiler.js
+++ project/tasks/compiler.js
@@ -2,108 +2,103 @@ module.exports = function(grunt) {
 
 	grunt.registerTask("vuecompile", "Compiles the Vue components into a single Javascript file", function() {
 	
+		const Tag = function(tagName, source) {
+			this.name          = tagName;
+			this.source        = source;
+			this.start         = "<"  + this.name + ">";
+			this.end	       = "</" + this.name + ">";
+			this.value         = "";
+			this.startOffset   = this.source.indexOf(this.start) + this.start.length;
+			this.endOffset     = this.source.indexOf(this.end);
+			this.value		   = this.source.substring(this.startOffset, this.endOffset).trim();
+			this.valueStripped = this.value.replace(/\s+/g,' ');
+			this.eval = function() {
+				return eval(this.value);
+			}
+		};
+		
+		const Component = function(fileName) {
+			this.fs       = require('fs');
+			this.toSource = require('tosource')
+			this.source   = this.fs.readFileSync(fileName, 'utf8');
+			this.tagSpec  = ["template", "script", "name", "top-level", "default"];
+			this.tags	  = {};
+			
+			this.getTags = function() {
+				this.tagSpec.forEach((tagName, index) => {
+					this.tags[tagName] = new Tag(tagName, this.source);
+				});
+			}
+			
+			this.compile = function() {
+				var scriptObj 		= eval("("+this.tags.script.value+")");
+				scriptObj.template  = this.tags.template.valueStripped;
+				return "const " + this.tags.name.value + " = " + this.toSource(scriptObj).replace(/\s+/g,' ') + ";\n";
+			}
+			
+			this.routes = function(defaultRouteExists) {
+				var topLevel     = this.tags["top-level"].eval();
+				if (!topLevel) return;
+				var isDefault    = this.tags["default"].eval();
+				var name         = this.tags.name.value;
+				var defaultRoute = {isDefault: true};
+				//What's up with preProcessedName?
+				//Our name is string, but we need our component name in the route obj 
+				//to be a JS symbol. However, because the component object isn't
+				//defined in the scope of the grunt task eval fails. To get around this
+				//I edit the string after converting the object to text, rather than before
+				var preProcessedName = "___"+name+"___";
+				var routeObj = {path: "/"+name, component: preProcessedName};		
+				if (!isDefault || defaultRouteExists) return [routeObj];
+				routeObj.meta = defaultRoute;
+				var defaultRouteObj = {path: "*", component: preProcessedName, meta: defaultRoute}
+				return [routeObj, defaultRouteObj];
+			}
+			
+			this.getTags();
+		};
+				
 		const Compiler = function() {
-		    this.toSource = require('tosource');
-		    this.fs = require('fs');
-		    this.componentDir = 'components/';
-		    this.compiledFile = this.componentDir+'compiled.js';
-		    this.encoding = 'utf8';
+		    this.toSource       = require('tosource');
+		    this.fs             = require('fs');
+		    this.componentDir   = 'components/';
+		    this.compiledFile   = this.componentDir+'compiled.js';
+		    this.routeFile      = "js/routes.js";
+		    this.encoding       = 'utf8';
 		    this.componentCount = 0;
-		    this.delims = {
-		        templateStart: "<template>",
-		        templateEnd: "</template>",
-		        scriptStart: "<script>",
-		        scriptEnd: "</script>",
-		        nameStart: "<name>",
-		        nameEnd: "</name>",
-		        topLevelStart: "<top-level>",
-		        topLevelEnd: "</top-level>",
-		        defaultStart: "<default>",
-		        defaultEnd: "</default>"
+		    this.routes         = [];
 		    
-		    };
-		    this.routes = [];
-		    
-		    this.componentName = function(sourceFile) {
-		    	return this.extractFromTags({doc: sourceFile, start: this.delims.nameStart, end: this.delims.nameEnd}).replace(/\s+/g,'');
-		    }
-		
-		    this.createComponentString = function(sourceFile) {
-		        var template = this.extractFromTags({doc: sourceFile, start: this.delims.templateStart, end: this.delims.templateEnd});
-		        var script   = this.extractFromTags({doc: sourceFile, start: this.delims.scriptStart, end: this.delims.scriptEnd});
-		        var name 	 = this.componentName(sourceFile); 
-		        var scriptObj = eval("(" + script + ")");
-		        scriptObj.template = this.reduceWhiteSpace(template);
-		        script = "const " + name + " = " + this.reduceWhiteSpace(this.toSource(scriptObj)) + ";\n";
-		        this.writeToOutputFile(script);
-		        console.log(name + ": compiled");
-		    };
-		
-		    this.createRoutes = function(sourceFile) {
-		    	var topLevel  = eval(this.extractFromTags({doc: sourceFile, start: this.delims.topLevelStart, end: this.delims.topLevelEnd}));
-		    	if (!topLevel) return;
-		    	
-		    	var name     = this.componentName(sourceFile);
-		    	var cName 	 = "___"+name+"___"; 
-		    	this.routes.push({path: "/"+name, component: cName});
-		    	
-		    	var isDefault = eval(this.extractFromTags({doc: sourceFile, start: this.delims.defaultStart, end: this.delims.defaultEnd}));
-		    	if (!isDefault) return;
-		    	
-		    	var defaultRouteCount = this.routes.filter((r) => r.path == "*").length
-		    	if (defaultRouteCount > 0) {
-		    		console.log("Warning: Multiple default components specified. Skipping new default route.");
-		    		return;
-		    	}
-		    	this.routes.push({path: "*", component: cName});
+		    this.defaultRouteExists = function() {
+		    	return !!this.routes.filter((r) => r.path == "*").length;
 		    }
 		    
 		    this.saveRoutes = function() {
 		    	var routeString = "var __VueZenRoutes = " + this.toSource(this.routes).replace(/"___/g, "").replace(/___"/g, "");
-		    	this.fs.writeFileSync("js/routes.js", routeString);
+		    	this.fs.writeFileSync(this.routeFile, routeString);
 		    }
 		    
 		    this.compile = function() {
 		        this.deleteOutputFile();
 		    	this.sourceFileList().forEach((fileName, index) => {
-		        	var sourceFile = this.readSourceFile(fileName);
-		        	this.createComponentString(sourceFile);
-		        	this.createRoutes(sourceFile);
+		    		var component   	= new Component(this.componentDir+fileName);
+		    		var componentJS 	= component.compile();
+		    		var componentRoutes = component.routes(this.defaultRouteExists());
+		    		this.routes = this.routes.concat(componentRoutes);
+		    		this.fs.appendFileSync(this.compiledFile, componentJS);
 		    	});
 		    	this.saveRoutes();
-		    };
+		    }
 		
 		    this.deleteOutputFile = function() {
-		        try {
-		            this.fs.unlinkSync(this.compiledFile);
-		        } catch(e) {
-		            console.log("Error deleting file: " + e)
-		        }
+		    	if (!this.fs.existsSync(this.compiledFile)) return;
+		    	this.fs.unlinkSync(this.compiledFile);
 		    }
-		
-		    this.extractFromTags = function(opts) {
-		        var start = opts.doc.indexOf(opts.start) + opts.start.length;
-		        var end   = opts.doc.indexOf(opts.end);
-		        return opts.doc.substring(start, end);
-		    }
-		
-		    this.readSourceFile = function(sourceFileName){
-		        return this.fs.readFileSync(this.componentDir+sourceFileName, this.encoding)
-		    }
-		
-		    this.reduceWhiteSpace = function(str) {
-		        return str.replace(/\s+/g,' ');
-		    } 
 		
 		    this.sourceFileList = function() {
 		        fileList = this.fs.readdirSync(this.componentDir);
 		        return fileList.filter((f) => f.indexOf(".vue") > -1);
 		    }
 		
-		    this.writeToOutputFile = function(compiledString) {
-		        result = this.fs.appendFileSync(this.compiledFile, compiledString)
-		        if (result) console.log("Error writing file: " + e);
-		    } 
 		}
 		
 		var compiler = new Compiler();
