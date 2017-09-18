032af3cc206b94a1042f35c7fcf6b4c42c0b6d0f
Adam Drew <adamrdrew@live.com>
2017-09-17 21:01:56 -0400

    New temporary test file

---
+++ project/components/newPage.vue
@@ -0,0 +1,36 @@
+<name>
+newPage
+</name>
+
+<template>
+	<div>
+		Hello World!
+	</div>
+</template>
+
+<script>
+{
+	data: function() {
+		return {
+		
+		};
+	},
+	methods: {
+	
+	},
+	computed: {
+	
+	},
+	mounted: {
+	
+	}
+}
+</script>
+
+<top-level>
+true
+</top-level>
+
+<default>
+false
+</default>
