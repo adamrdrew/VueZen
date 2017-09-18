1f14a76c0a4c111f2e80796df8c31dff8a58ebc1
Adam Drew <adamrdrew@live.com>
2017-09-17 21:01:40 -0400

    Routes are now moved into their own file

---
+++ project/js/routes.js
@@ -0,0 +1,8 @@
+var __VueZenRoutes = [ { path:"/newPage",
+    component:newPage },
+  { path:"/pageOne",
+    component:pageOne },
+  { path:"*",
+    component:pageOne },
+  { path:"/pageTwo",
+    component:pageTwo } ]
