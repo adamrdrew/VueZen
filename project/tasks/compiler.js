cac94fab1f1cd1ac4cf5c497d8932599b43059ce
Adam Drew <adamrdrew@live.com>
2017-09-17 21:01:06 -0400

    Added support for route building to compiler

--- project/tasks/compiler.js
+++ project/tasks/compiler.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
 
-	grunt.registerTask("vuecompile", function() {
+	grunt.registerTask("vuecompile", "Compiles the Vue components into a single Javascript file", function() {
 	
 		const Compiler = function() {
 		    this.toSource = require('tosource');
@@ -15,14 +15,23 @@ module.exports = function(grunt) {
 		        scriptStart: "<script>",
 		        scriptEnd: "</script>",
 		        nameStart: "<name>",
-		        nameEnd: "</name>"
+		        nameEnd: "</name>",
+		        topLevelStart: "<top-level>",
+		        topLevelEnd: "</top-level>",
+		        defaultStart: "<default>",
+		        defaultEnd: "</default>"
 		    
 		    };
+		    this.routes = [];
+		    
+		    this.componentName = function(sourceFile) {
+		    	return this.extractFromTags({doc: sourceFile, start: this.delims.nameStart, end: this.delims.nameEnd}).replace(/\s+/g,'');
+		    }
 		
 		    this.createComponentString = function(sourceFile) {
 		        var template = this.extractFromTags({doc: sourceFile, start: this.delims.templateStart, end: this.delims.templateEnd});
 		        var script   = this.extractFromTags({doc: sourceFile, start: this.delims.scriptStart, end: this.delims.scriptEnd});
-		        var name     = this.extractFromTags({doc: sourceFile, start: this.delims.nameStart, end: this.delims.nameEnd});
+		        var name 	 = this.componentName(sourceFile); 
 		        var scriptObj = eval("(" + script + ")");
 		        scriptObj.template = this.reduceWhiteSpace(template);
 		        script = "const " + name + " = " + this.reduceWhiteSpace(this.toSource(scriptObj)) + ";\n";
@@ -30,12 +39,38 @@ module.exports = function(grunt) {
 		        console.log(name + ": compiled");
 		    };
 		
+		    this.createRoutes = function(sourceFile) {
+		    	var topLevel  = eval(this.extractFromTags({doc: sourceFile, start: this.delims.topLevelStart, end: this.delims.topLevelEnd}));
+		    	if (!topLevel) return;
+		    	
+		    	var name     = this.componentName(sourceFile);
+		    	var cName 	 = "___"+name+"___"; 
+		    	this.routes.push({path: "/"+name, component: cName});
+		    	
+		    	var isDefault = eval(this.extractFromTags({doc: sourceFile, start: this.delims.defaultStart, end: this.delims.defaultEnd}));
+		    	if (!isDefault) return;
+		    	
+		    	var defaultRouteCount = this.routes.filter((r) => r.path == "*").length
+		    	if (defaultRouteCount > 0) {
+		    		console.log("Warning: Multiple default components specified. Skipping new default route.");
+		    		return;
+		    	}
+		    	this.routes.push({path: "*", component: cName});
+		    }
+		    
+		    this.saveRoutes = function() {
+		    	var routeString = "var __VueZenRoutes = " + this.toSource(this.routes).replace(/"___/g, "").replace(/___"/g, "");
+		    	this.fs.writeFileSync("js/routes.js", routeString);
+		    }
+		    
 		    this.compile = function() {
 		        this.deleteOutputFile();
 		    	this.sourceFileList().forEach((fileName, index) => {
 		        	var sourceFile = this.readSourceFile(fileName);
 		        	this.createComponentString(sourceFile);
-		        })
+		        	this.createRoutes(sourceFile);
+		    	});
+		    	this.saveRoutes();
 		    };
 		
 		    this.deleteOutputFile = function() {
