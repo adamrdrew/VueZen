ac49ce7a8803f20e67371db7fcdd7824bf1e39bc
Adam Drew <adamrdrew@live.com>
2017-09-19 19:12:22 -0400

    Made the component template more useful. It generates an active page now

--- project/lib/template.vue
+++ project/lib/template.vue
@@ -3,9 +3,16 @@
 <default>{{isDefault}}</default>
 
 <template>
-	<div>
-		Hello World!
+<div>
+	<div class="ui-page ui-page-active">
+		<header class='ui-header'>
+			<h2 class='ui-title'>VueZen</h2>
+		</header>
+		<div class='ui-content content-padding'>
+			<p>Hello World!</p>
+		</div>
 	</div>
+</div>
 </template>
 
 <script>
